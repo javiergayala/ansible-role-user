340d1b4ce95647f5da8f164a43d77f390fb714c1
Javier Ayala <jayala@rackspace.com>
2016-11-01 10:22:59 -0500

    feat(test): add tests for user creation

---
+++ tests/functional/a/tests/test_users.py
@@ -0,0 +1,20 @@
+import pytest
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('inventory').get_hosts('pci')
+
+
+# @pytest.mark.docker_images("centos:7")
+@pytest.mark.docker_images("node-a")
+@pytest.mark.parametrize("username", ["deployment", "user1"])
+def test_pci_user_creation(User, File, username):
+    userchk = User(username)
+    sshauthkey = File('/home/%s/.ssh/authorized_keys' % username)
+    assert userchk.exists
+    assert userchk.name == username
+    assert userchk.home == "/home/%s" % username
+    assert sshauthkey.exists
+    assert sshauthkey.mode == 0o600
+    assert sshauthkey.user == username
+    assert sshauthkey.group == username
+    assert sshauthkey.contains("RdK8jlqm8tehUc9c9WhQ==")
