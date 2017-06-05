32eeef0fe571b90cda3b0cb10e31ff9e3aa49a2e
Javier Ayala <javier.g.ayala@gmail.com>
2017-06-05 13:46:22 -0500

    feat(bash): create a uniform bash prompt

---
+++ tests/functional/a/tests/test_prompt.py
@@ -0,0 +1,26 @@
+import pytest
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('inventory').get_hosts('pci')
+
+
+# @pytest.mark.docker_images("centos:7")
+@pytest.mark.docker_images("node-a")
+def test_bash_prompt(File, Command):
+    cmd = Command("hostname")
+    prompt = File("/etc/profile.d/Z99-bash_prompt.sh")
+    test_string = """
+    PS1="\[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;196m\]\u\
+    [$(tput sgr0)\]\[\033[38;5;15m\]@\h-\[$(tput sgr0)\]
+    \[\033[38;5;14m\]TEST\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput bold)\]
+    \[$(tput sgr0)\]\[$(tput bold)\]\[\033[38;5;196m\]|PROD|
+    \[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\]:\n\[$(tput sgr0)\]
+    \[\033[38;5;6m\][\w]\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;46m\]>
+    \[$(tput sgr0)\]\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
+    """
+    print("%s", prompt.content_string)
+    print("Hostname: %s", cmd.stdout)
+    assert prompt.contains(test_string)
+    assert prompt.user == "root"
+    assert prompt.group == "root"
+    assert prompt.mode == 0o644
